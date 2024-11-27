fdd5ed6d305a24b667791b5f48795ceae5933c36
UdayKunta <105340365+UdayKunta@users.noreply.github.com>
2024-11-27 16:42:48 +0530

    Update provider.tf

--- provider.tf
+++ provider.tf
@@ -10,8 +10,8 @@ terraform {
 
 provider "azurerm" {
   features {}
-  client_id       = "<appId>"
-  client_secret   = "<password>"
+  client_id       = "d5873f0d-cc02-4190-bfc9-5b5428e68158"
+  client_secret   = "SOc8Q~UY7WIzpRcfQWP.Cv1ucu5G0E_GWTOdocaM"
   tenant_id       = "0d1e47cc-960b-4576-82cc-e3e94f5e83bf"
   subscription_id = "511235a9-1907-41f1-b32f-27948e2a86be"
 }
