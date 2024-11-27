8a06fa3c50d0e6fbc27e457ea7115caaac3bff83
UdayKunta <105340365+UdayKunta@users.noreply.github.com>
2024-11-27 14:20:46 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,7 +1,7 @@
 resource "azurerm_storage_account" "storage" {
-  name                     = "storagetfminimal"
-  resource_group_name      = "rg-fsapps-backstage"
-  location                 = "East US"
-  account_tier             = "Standard"
-  account_replication_type = "LRS"
+  name                     = var.sa_name //"storagetfminimal"
+  resource_group_name      = "rg-fsapps-backstage" //var.sa_rgname
+  location                 = var.sa_location //"East US"
+  account_tier             = var.sa_account_tier //"Standard"
+  account_replication_type = var.sa_account_replication_type //"LRS"
 }
