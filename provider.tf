ac22b37ac0c6422c229870b7afcc16bd59f2239e
UdayKunta <105340365+UdayKunta@users.noreply.github.com>
2024-11-27 12:21:57 +0530

    Create provider.tf

---
+++ provider.tf
@@ -0,0 +1,7 @@
+provider "azurerm" {
+  features {}
+  client_id       = "<appId>"
+  client_secret   = "<password>"
+  tenant_id       = "0d1e47cc-960b-4576-82cc-e3e94f5e83bf"
+  subscription_id = "511235a9-1907-41f1-b32f-27948e2a86be"
+}
