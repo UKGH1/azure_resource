7b0fa5877fd57c9c859e26db973e69533fa63223
UdayKunta <105340365+UdayKunta@users.noreply.github.com>
2024-12-06 12:32:41 +0530

    Updated

--- provider.tf
+++ provider.tf
@@ -3,7 +3,7 @@ terraform {
     resource_group_name  = "rg-fsapps-backstage"
     storage_account_name = "devbackstagepoclogsmnfpq"
     container_name       = "tfstate-viabackstage"
-    key                  = "${var.tfstate_file_name}" //"stgacct.tfstate"
+    //key                  = "${var.tfstate_file_name}" //"stgacct.tfstate"
   }
   required_providers {
     azurerm = {
