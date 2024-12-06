c1f791608bd5842911ddccd34d1e2bf1ec088d51
UdayKunta <105340365+UdayKunta@users.noreply.github.com>
2024-12-06 12:12:06 +0530

    Updated with remote backend for tfstate file

--- provider.tf
+++ provider.tf
@@ -1,11 +1,16 @@
 terraform {
+  backend "azurerm" {
+    resource_group_name  = "rg-fsapps-backstage"
+    storage_account_name = "devbackstagepoclogsmnfpq"
+    container_name       = "tfstate-viabackstage"
+    key                  = "stgacct.tfstate"
+  }
   required_providers {
     azurerm = {
       source  = "hashicorp/azurerm"
       version = "3.91.0"
     }
   }
-  #required_version = "=1.5.5"
 }
 
 provider "azurerm" {
